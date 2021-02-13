a2d19bc16ff4d35f7476d9123ca6f5481ddaa16b
elena-vost <angara1298@gmail.com>
2021-02-13 14:46:52 +0100

    GIFand text Number Picker

--- Projects/JSX/index.js
+++ Projects/JSX/index.js
@@ -4,14 +4,19 @@ function getNum() {
 class NumPicker extends React.Component {
     render (){
         const num = getNum();
+        let msg;
+        if(num === 7) {
+            msg = <div>
+                <h2>CONGRATS YOU WIN!</h2>
+                <img src ="https://i.pinimg.com/originals/8d/a3/cc/8da3cc2c3db6b45b56eb24b622c44c11.gif" />
+            </div>
+        } else {
+            msg = <p>SORRY! YOU LOSE</p>
+        }
         return (
             <div>
                 <h1>Your number is: {  num} </h1>
-                <p>{num === 7 ? 'Congrats!' : 'Unlucky!'}</p>
-                {
-                    num === 7 
-                    ? <img src="https://i.pinimg.com/originals/8d/a3/cc/8da3cc2c3db6b45b56eb24b622c44c11.gif" /> 
-                    : null}
+                    {msg}
             </div>
         );
     }
